32589b25d50079e7495f27375e8c4fbb4f5d7c3e
pankaj5252 <pbkale25@gmail.com>
2024-06-22 14:07:13 +0530

    Update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
 
   return (
     <>
-      <header className="bg-customBlue shadow-md relative text-white">
-        <div className="container-fluid mx-auto px-4 py-2 flex justify-between items-center flex-wrap">
+      <header className="bg-customBlue shadow-md text-white sticky top-0">
+        <div className="container  mx-auto px-4 py-2 flex justify-between items-center flex-wrap">
           <div className="flex items-center">
             <button
               onClick={() => setIsOpen(!isOpen)}
@@ -62,19 +62,19 @@ const Header = () => {
               <b className='text-xs lg:text-base'>Account & Lists</b>
             </button>
           </div>
-          <div className="hidden lg:block ml-4 p-1 hover:border-b">
+          <div className="hidden lg:block  ml-4 p-1 hover:border-b">
             <button>
               <p className='text-xs lg:text-sm'>Returns</p>
               <b className='text-xs lg:text-base'>& Orders</b>
             </button>
           </div>
-          <div className="p-2">
+          <div className="p-2 ">
             <div className='md:hidden inline-flex'>
               <p className='mr-3'>Sign in</p>
               <button className=' mr-5'><i className="fa-solid fa-user text-white"></i></button>
             </div>
             <button><i className="fa-solid fa-cart-shopping w-6 lg:w-8"></i></button>
-            <span className='text-xs hidden lg:inline-flex lg:text-xl'>Cart</span>
+            <span className='text-xs hidden lg:inline-block lg:text-xl'>Cart</span>
           </div>
         </div>
         <div className='sm:hidden inline-flex items-center bg-gray-200 rounded-md mx-auto overflow-hidden h-11 w-full max-w-3xl'>
@@ -88,25 +88,25 @@ const Header = () => {
 
       {isOpen && (
         <div className="w-64 lg:hidden absolute top-0 bg-gray-100 text-black p-2">
-           <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="text-dark focus:outline-none lg:hidden"
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-dark focus:outline-none lg:hidden"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-                />
-              </svg>
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+              />
+            </svg>
+          </button>
           <Link to="/" className="block py-1 border-b">Home</Link>
           <Link to="/about" className="block py-1 border-b">About</Link>
           <Link to="/services" className="block py-1 border-b">Services</Link>
